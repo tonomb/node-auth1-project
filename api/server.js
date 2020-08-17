6484bd90af8b7a4194968ef851cc64cd06c4c2ef
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-17 17:46:08 -0500

    implemented user sessions

--- api/server.js
+++ api/server.js
@@ -1,15 +1,41 @@
-const express = require('express')
-const server = express()
+const express = require("express");
+const server = express();
+const session = require("express-session");
+const KnexSessionStore = require('connect-session-knex')(session);
 
-const usersRouter = require('./users/userRouter')
+const usersRouter = require("./users/userRouter");
+const authRouter = require("./auth/authRouter");
+const protected = require('./auth/protected')
+const dbConnection = require('../data/dbConfig')
 
-server.use(express.json())
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 10, //after 10 min the cookie expires
+    secure: process.env.COOKIE_SECURE || false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true, //gdpr compliance, the client should drive this
+  store: new KnexSessionStore({ //create sessions table in db.
+    knex: dbConnection,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 //delete expired sessions every hour
+  })
+};
 
-server.use('/users', usersRouter)
-
-server.get('/', (req, res) => {
-  res.status(200).json({api: 'running on port 5000'})
-})
+server.use(express.json());
+server.use(session(sessionConfig));
 
 
-module.exports = server
+server.use("/users", protected, usersRouter);
+server.use('/api', authRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running on port 5000" });
+});
+
+module.exports = server;
--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -14,42 +14,5 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/register', (req, res) => {
-  const {password, username} = req.body
-
-  const hash = bcrypt.hashSync(password, 8);
-
-  const newUser = {
-    username,
-    password: hash
-  }
-
-  Users.add(newUser)
-    .then( user =>{
-      res.status(201).json(user)
-    })
-    .catch( err =>{
-      console.log(err);
-      res.status(500).json({message: 'something went wrong when saving the new user to the db'})
-    })
-})
-
-
-router.post('/login', (req, res) => {
-  const {password, username} = req.body
-
-  Users.getByUsername(username)
-    .then( user =>{
-      if(user && bcrypt.compareSync(password, user.password)){
-        res.status(200).json({message: 'welcome'})
-      } else {
-        res.status(401).json({error:'invalid credentials'})
-      }
-    })
-    .catch( err =>{
-      console.log(err);
-      res.status(500).json({error: err.message})
-    })
-})
 
 module.exports = router
