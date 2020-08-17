ee6549bb6b2626d8f337cf63470a393c06a5be31
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-17 17:15:09 -0500

    login and register routes setup, hashing passwords

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -2,9 +2,22 @@ const db = require('../../data/dbConfig')
 
 module.exports = {
   add,
+  getUsers,
+  getByUsername
 }
 
 
-function add(){
-  
+function add(user){
+  return db('users').insert(user)
+    .then(ids => {
+      return db('users').select('*').where({id: ids}).first()
+    })
+}
+
+function getUsers(){
+  return db('users').select('*')
+}
+
+function getByUsername(username){
+  return db('users').select('*').where({username}).first()
 }
--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -1,12 +1,55 @@
 const express = require('express')
-
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+
+const Users = require('./userModel')
 
 //==== /users ======
 
 router.get('/', (req, res) => {
-  res.status(200).json({message: 'users'})
+  Users.getUsers()
+    .then(users =>{
+      res.status(200).json({users})
+    })
 })
 
+router.post('/register', (req, res) => {
+  const {password, username} = req.body
+
+  const hash = bcrypt.hashSync(password, 8);
+
+  const newUser = {
+    username,
+    password: hash
+  }
+
+  Users.add(newUser)
+    .then( user =>{
+      res.status(201).json(user)
+    })
+    .catch( err =>{
+      console.log(err);
+      res.status(500).json({message: 'something went wrong when saving the new user to the db'})
+    })
+})
+
+
+router.post('/login', (req, res) => {
+  const {password, username} = req.body
+
+  Users.getByUsername(username)
+    .then( user =>{
+      if(user && bcrypt.compareSync(password, user.password)){
+        res.status(200).json({message: 'welcome'})
+      } else {
+        res.status(401).json({error:'invalid credentials'})
+      }
+    })
+    .catch( err =>{
+      console.log(err);
+      res.status(500).json({error: err.message})
+    })
+})
 
 module.exports = router
